ad2daee26fc490a7a9e95e04856a7d6633501efd
linem davton <linem.davton@gmail.com>
2025-08-22 14:52:04 +0200

    Macros

--- exercises/21_macros/macros4.rs
+++ exercises/21_macros/macros4.rs
@@ -3,10 +3,11 @@
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };
+
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
-    }
+    };
 }
 
 fn main() {
